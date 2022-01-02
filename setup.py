1b3af9491b57b71e9209ae37be0e94c8b3acbcab
mat <27899617+mat-1@users.noreply.github.com>
2022-01-02 15:59:33 -0600

    1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
 	name='repltalk',
-	version='1.0.0',
+	version='1.0.1',
 	author='mat',
 	author_email='pypi@matdoes.dev',
 	description='Allows you to do various things with the kind of unofficial Repl.it Repl Talk API',
